e210c9df8ae70b2694a87d1ff5791fd2c4bf2da8
GekJack <mma180905@gmail.com>
2023-12-24 00:14:38 +0200

    чистка main

--- LabOOP4/main.cpp
+++ LabOOP4/main.cpp
@@ -2,16 +2,6 @@
 #include "class.h"
 
 int main(){
-    String<int> a(56, 5);
-    a.OutPut();
-    String<char> b('f', 5);
-    b.OutPut();
-    String<int> c(int(), 12);
-    b += a;
-    b.OutPut();
-
-
-    
     
     return 0;
 }
