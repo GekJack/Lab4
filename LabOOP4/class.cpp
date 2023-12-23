1dc73f4e59ca8621ba11dbd2013527debcc1b93e
GekJack <mma180905@gmail.com>
2023-12-23 20:05:09 +0200

    clear

--- LabOOP4/class.cpp
+++ LabOOP4/class.cpp
@@ -4,21 +4,21 @@
 
 
 template <typename T>
-String<T> :: String(const String&& other) :
+String<T> :: String(const String&& other) : //Конструктор переміщення
 	mass_char(other.mass_char), len(other.len)
 {
 	other.len = 0;
 	other.mass_char = nullptr;
 }
 template <typename T>
-String<T> :: ~String() {
+String<T> :: ~String() { // Деструктор
 	if (this->mass_char) {
 		delete[] this->mass_char;
 	}
 	this->len = 0;
 }
 template <typename T>
-String<T>::String(T value, int len) {
+String<T>::String(T value, int len) { // Конструктор одного значення
 	this->mass_char = new T[len];
 	for (int i = 0; i < len; i++) {
 		this->mass_char[i] = value;
@@ -26,7 +26,7 @@ String<T>::String(T value, int len) {
 }
 
 template <typename T>
-String<T> :: String() {
+String<T> :: String() { // Конструктор за замовченням
 	this->len = 10;
 	this->mass_char = new T[len];
 	for (int i = 0; i < this->len; i++) {
--- LabOOP4/main.cpp
+++ LabOOP4/main.cpp
@@ -2,13 +2,5 @@
 #include "class.h"
 using namespace std;
 int main(){
-    cout << "Laba 4" << endl;
-    cout << "Laba 4" << endl;
-    cout << "hello" << endl;
-    int a = 5;
-    int b = a;
-    int c = a + b;
-    // huy
-    String<char> obj;
     return 0;
 }
