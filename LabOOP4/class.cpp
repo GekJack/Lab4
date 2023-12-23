b51a92aeee75b43ab0e193c6194ad145c74b4ac5
GekJack <mma180905@gmail.com>
2023-12-23 21:49:25 +0200

    Метод:  Отримання символа за його позицією в строці Отримання довжини строки Конструктор копирования

--- LabOOP4/class.cpp
+++ LabOOP4/class.cpp
@@ -24,7 +24,14 @@ String<T> :: String(T value, int len) { // Конструктор одного значення
 		this->mass_char[i] = value;
 	}
 }
-
+template <typename T>
+template <typename U>
+String<T> :: String(const String<U>& other){ // Конструктор копирования
+	this->mass_char = new T[other.GetLen()];
+	for (int i = 0; i < this->len; i++) {
+		this->mass_char[i] = static_cast<T>(other.GetCurrentSymbol());
+	}
+}
 template <typename T>
 String<T> :: String() { // Конструктор за замовченням
 	this->len = 10;
@@ -32,5 +39,13 @@ String<T> :: String() { // Конструктор за замовченням
 	for (int i = 0; i < this->len; i++) {
 		mass_char[i] = 'a';
 	}
+}
+template <typename T> 
+T String<T> ::GetCurrentSymbol(int pos) { // Отримання символа за його позицією в строці
+	return this->mass_char[pos];
+}
+template <typename T>
+int String<T> :: GetLen() { // Отримання довжини строки
+	return this->len;
 }
 #endif //class_cpp
--- LabOOP4/class.h
+++ LabOOP4/class.h
@@ -7,9 +7,13 @@ private:
     T* mass_char;
     int len;
 public:
+    int GetLen();
+    T GetCurrentSymbol(int pos);
     String();
     String(T value, int len);
     String(const String&& other);
+    template <typename U>
+    String(const String<U>& other);
     ~String();
 };
 #include "class.cpp"
