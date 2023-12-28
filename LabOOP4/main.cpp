ee180bda2e61eb59c16ff6c404ece90c5fa7b90d
GekJack <mma180905@gmail.com>
2023-12-28 22:55:03 +0200

    Контактенация строк

--- LabOOP4/main.cpp
+++ LabOOP4/main.cpp
@@ -2,9 +2,8 @@
 #include "class.h"
 
 int main() {
-    char mass[5] = {'5','4','3','2','1'};
-    String<char> a(&mass[2], &mass[4]);
-    String<int> b(56, 5);
+    String<int> a(1, 4);
+    String<int> b(2, 5);
     a += b;
     a.OutPut();
     return 0;
