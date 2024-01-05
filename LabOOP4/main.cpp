8255fa9f4c29af87947e6696d25b7a954312eeae
GekJack <mma180905@gmail.com>
2024-01-05 18:25:23 +0200

    5

--- LabOOP4/main.cpp
+++ LabOOP4/main.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
 #include "class.h"
 int main() {
-    String<int> a(112, 5);
-    String<char> b('w', 6);
-    a.OutPut();
-    b.OutPut();
-    int arr[5] = { 1, 2, 3, 4, 5 };
-    a = CreateString<int, int>(&arr[0], &arr[4]);
-    a.OutPut();
     return 0;
 }
